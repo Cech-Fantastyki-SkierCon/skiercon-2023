05688be839d8a70fa647da9c0e6907c5a4e2898f
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2023-05-08 02:41:32 +0200

    Add rollbar

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,8 @@ interface ImportMetaEnv {
   readonly PUBLIC_API_URL: string
   readonly PUBLIC_RECAPTCHA_SITE_KEY: string
   readonly PUBLIC_GTM: string
+  readonly PUBLIC_ROLLBAR: string
+  readonly SECRET_ROLLBAR: string
 }
 
 interface ImportMeta {
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,20 @@
+import child_process from 'node:child_process'
+
 export function fixOrphans(text: string): string {
   return text.replace(/ (.) /g, ' $1&nbsp;')
+}
+
+let gitRevision = ''
+export function getGitRevision() {
+  if (gitRevision) {
+    return gitRevision
+  }
+
+  gitRevision = child_process
+    .execSync('git rev-parse HEAD')
+    .toString()
+    .trim()
+    .slice(0, 7)
+
+  return gitRevision
 }
