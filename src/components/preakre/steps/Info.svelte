85105bc7903ee51015e8798dd07f7c2787c29796
Dominika Szymanska <dominika1872@gmail.com>
2023-05-23 00:00:47 +0200

    Fix smol typo

--- src/components/preakre/steps/Info.svelte
+++ src/components/preakre/steps/Info.svelte
@@ -66,7 +66,7 @@
     </p>
     <p class="my-6 alert alert-info shadow-lg">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current flex-shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-      Pszczelego Patrona nie obejmują zniżki na akredytację (jako Twórcy Programu lub uczestnika konkusu JaSkier/OSkier). 
+      Pszczelego Patrona nie obejmują zniżki na akredytację (jako Twórcy Programu lub uczestnika konkursu JaSkier/OSkier). 
     </p>
 
     <div class="divider" />
